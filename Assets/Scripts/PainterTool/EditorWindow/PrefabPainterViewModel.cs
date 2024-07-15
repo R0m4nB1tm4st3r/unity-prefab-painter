ff46e69991c37b6e0880d76912b9db1043748dc2
Roman Hochhalter <romanhochhalter1@web.de>
2024-07-15 14:11:34 +0200

    register paintings for undoing

--- Assets/Scripts/PainterTool/EditorWindow/PrefabPainterViewModel.cs
+++ Assets/Scripts/PainterTool/EditorWindow/PrefabPainterViewModel.cs
@@ -9,6 +9,10 @@ namespace PainterTool.EditorWindow
     public class PrefabPainterViewModel
     {
         private const string NoPrefabError = "There is no Prefab selected!";
+        private const string CreateObjectDescription = "Create Object";
+        private const string CreatePrefabInstanceDescription = "Create Prefab Instance";
+        private const string CreateMultipleObjectsDescription = "Create multiple Objects";
+        private const string CreateMultiplePrefabInstancesDescription = "Create multiple Prefab Instances";
         
         public PrefabPainterModel Model { get; private set; } = new();
 
@@ -55,17 +59,31 @@ namespace PainterTool.EditorWindow
             {
                 case PrefabAssetType.NotAPrefab:
                 case PrefabAssetType.MissingAsset:
+                    // create Object
                     var obj = Object.Instantiate(Model.SinglePrefab, painterInfo.point, Quaternion.identity);
+                    
+                    // register Object for Undo Event
+                    Undo.RegisterCreatedObjectUndo(obj, CreateObjectDescription);
+                    
+                    // set Normal and Scale
                     obj.transform.up = painterInfo.normal;
                     obj.transform.localScale = Model.ScaleFactor;
+                    
                     return;
                 case PrefabAssetType.Regular:
                 case PrefabAssetType.Model:
                 case PrefabAssetType.Variant:
+                    // create Prefab Instance
                     var prefObj = (GameObject)PrefabUtility.InstantiatePrefab(Model.SinglePrefab);
+                    
+                    // register Prefab Instance for Undo Event
+                    Undo.RegisterCreatedObjectUndo(prefObj, CreatePrefabInstanceDescription);
+                    
+                    // set Position, Normal and Scale
                     prefObj.transform.position = painterInfo.point;
                     prefObj.transform.up = painterInfo.normal;
                     prefObj.transform.localScale = Model.ScaleFactor;
+                    
                     return;
                 default:
                     throw new ArgumentOutOfRangeException();
